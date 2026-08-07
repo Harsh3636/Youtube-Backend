eb860c6104156c7d60bfa3ab3d9ba93c54fa6451
Harsh <suhanichaudhary7906@gmail.com>
2026-08-07 22:48:07 +0530

    utils code

--- Readme.md
+++ Readme.md
@@ -1,3 +1,4 @@
 # Chai or BAckend Series.
 
 In these project we make the backend of youtube.
+//app.js hum jb use krte hai tb hume sirf middleware ya fir configuration setings krni hai.
--- Src/index.js
+++ Src/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 //NOW WE LEARN SECOND METHOD OF STROING DATA OR CALL IT IN DATABASE.
 
 dotenv.config({
@@ -12,9 +13,23 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+    app.on("error", (error) => {
+    console.log("Errr: ", error);
+    throw error;
+})
+    //agr server nhi mila to is port pe run hojyega project lkin crash nhi hoga.
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at port: ${process.env.PORT}`);
+    })
+})
+
+.catch((error) => {
+    console.log("MONGO db connection failed ", error);
+})
 
 
-
+  
 
 
 
